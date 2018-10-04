e0818c9e3017bd1b95c65573c7b3395e12e1bce8
wangui.njoka <wangui.njoka@gmail.com>
2018-10-04 11:51:11 +0300

    connected postgres

--- instagram/models.py
+++ instagram/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class Image(models:Model):
+class Image(models.Model):
     location = models.CharField(max_length = 60)
     picture = models.ImageField(upload_to = 'images/', blank=True)
-    caption = model.TextField(blank=True)
+    caption = models.TextField(blank=True)
 
 
     def save_image(self):
-		self.save()
-	def update_image(self):
-		self.update()
-	def delete_image(self):
-		self.delete()
+	    self.save()
+    def update_image(self):
+        self.update()
+    def delete_image(self):
+        self.delete()
 
     @classmethod
     def display_images(cls):
