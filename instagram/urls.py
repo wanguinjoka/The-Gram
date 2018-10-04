9e9eaefb87a9c437d5e759ba95ee489f90a79669
wangui.njoka <wangui.njoka@gmail.com>
2018-10-04 19:35:04 +0300

    implemented registration

--- instagram/urls.py
+++ instagram/urls.py
@@ -5,6 +5,7 @@ from . import views
 
 urlpatterns=[
     url('^$', views.all_images, name ='allimages'),
+    
     ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
--- instagram/views.py
+++ instagram/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from .models import Image,Comment
 
 # Create your views here.
+@login_required(login_url='/accounts/login/')
 def all_images(request):
     images = Image.display_images()
 
